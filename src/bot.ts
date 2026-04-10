82d5dd91fbdf7dbe50768a766478d9836b7a8f30
Mekmmmm <86698989+Mekmmmm@users.noreply.github.com>
2026-04-10 15:02:53 +0800

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -9,6 +9,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.DirectMessages,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
   ],
   partials: [Partials.Channel]
 });
